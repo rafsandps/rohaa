6c183edfac0a50bfa7e502a2f7bb217a7936aa72
rafsandps <77680859+rafsandps@users.noreply.github.com>
2024-12-10 14:58:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    document.body.classList.remove('not-loaded');
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
   }, 1000);
-});
+};
